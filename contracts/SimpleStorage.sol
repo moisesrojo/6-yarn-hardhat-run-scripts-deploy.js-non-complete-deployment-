2c8a17ffdfc7af0dc5860854a8466558374a8713
moisesrojo <moisesrojortega@gmail.com>
2022-12-13 08:32:28 +0100

    new-commit

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,34 +1,30 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
 
-pragma solidity 0.8.8;
-
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
 
 contract SimpleStorage {
-  uint256 favoriteNumber;
-
-  struct People {
     uint256 favoriteNumber;
-    string name;
-  }
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    People[] public people;
 
-  // uint256[] public anArray;
-  People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-  mapping(string => uint256) public nameToFavoriteNumber;
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 
-  function store(uint256 _favoriteNumber) public {
-    favoriteNumber = _favoriteNumber;
-  }
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
 
-  function retrieve() public view returns (uint256) {
-    return favoriteNumber;
-  }
-
-  function addPerson(string memory _name, uint256 _favoriteNumber) public {
-    people.push(People(_favoriteNumber, _name));
-    nameToFavoriteNumber[_name] = _favoriteNumber;
-  }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
